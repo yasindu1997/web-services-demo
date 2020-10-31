17bbc51138a21d7ba23ef50b9d281f4c3d5d16e9
yasindu1997 <yasindusathsaraoth@gmail.com>
2020-10-31 16:13:08 +0530

    Add Message POJO

---
+++ src/main/java/lk/ijse/wsdemo/data/Message.java
@@ -0,0 +1,33 @@
+package lk.ijse.wsdemo.data;
+
+import java.util.Date;
+
+public class Message {
+    private String sender;
+    private String message;
+    private Date sentDate;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(Date sentDate) {
+        this.sentDate = sentDate;
+    }
+}
