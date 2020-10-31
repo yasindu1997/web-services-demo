5bf5db59a5cc95c8d6f5d350c40e437684403a70
yasindu1997 <yasindusathsaraoth@gmail.com>
2020-10-31 14:32:12 +0530

    Init spring boot project

---
+++ src/main/java/lk/ijse/wsdemo/WsDemoApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.wsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/wsdemo/WsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.wsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
