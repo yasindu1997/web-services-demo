4ac9dd41ac15d48eab1ce4b8c2f682796fd7b07d
yasindu1997 <yasindusathsaraoth@gmail.com>
2020-10-31 15:28:56 +0530

    Change spring boot starter library to start web

---
+++ src/main/java/lk/ijse/wsdemo/MyRestController.java
@@ -0,0 +1,17 @@
+package lk.ijse.wsdemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @GetMapping(path = "/hello", produces = "text/plain")
+    public ResponseEntity<Object> sayHello() {
+        return new ResponseEntity<Object>("Hello World!", HttpStatus.OK);
+    }
+}
